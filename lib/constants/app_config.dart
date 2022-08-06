7f3ecb6c6035276c752d5bf46c200b0ef66f79ee
Khofidin <offiedz@gmail.com>
2022-08-06 22:13:21 +0700

    Move units setting into config

--- lib/constants/app_config.dart
+++ lib/constants/app_config.dart
@@ -4,6 +4,7 @@ class AppConfig {
   static String openWeatherApiKey = dotenv.get('OPEN_WEATHER_API_KEY');
   static const String openWeatherApiDomain = "api.openweathermap.org";
   static const String openWeatherIconUrl = "https://openweathermap.org/img/wn/";
+  static const String units = "metric"; //metric, imperial
   static const int photoRefresh = 30; //seconds
   static const int currentWeatherRefresh = 30; //minutes
   static const int forecastRefresh = 1; //hours
