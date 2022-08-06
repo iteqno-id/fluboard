2b8a20832046c0658e12ea23f6dbd73f5642678f
Khofidin <offiedz@gmail.com>
2022-08-06 21:55:07 +0700

    Revamp clock

--- lib/utils/extension.dart
+++ lib/utils/extension.dart
@@ -2,6 +2,8 @@ import 'package:intl/intl.dart';
 
 extension DateTimeExtension on DateTime {
   String asHHMM() => DateFormat('HH:mm').format(this);
+  String ashmm() => DateFormat('h:mm').format(this);
+  String asAmPm() => DateFormat('a').format(this);
   String asSecond() => DateFormat('ss').format(this);
   String asDateMonthYear() => DateFormat('d MMMM yyyy').format(this);
   String asDay() => DateFormat('EEEE').format(this);
