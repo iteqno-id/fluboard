504aac4402a1412a72e2b323e5c8ef15ccf87d06
Khofidin <offiedz@gmail.com>
2022-08-07 17:33:53 +0700

    ZPrepare settings screen

---
+++ lib/screen/settings_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Settings"),
+      ),
+    );
+  }
+}
